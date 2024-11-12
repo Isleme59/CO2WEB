7784404fb1995ce79a732da8aa1bf749c9119847
GoutteDooo <goudal.Donovan@live.fr>
2024-11-12 22:01:55 +0100

    countdown finished

--- js/countdown.js
+++ js/countdown.js
@@ -2,28 +2,27 @@ const day = document.getElementById("countdown-day");
 const hours = document.getElementById("countdown-hours");
 const minutes = document.getElementById("countdown-minutes");
 const seconds = document.getElementById("countdown-seconds");
-const countdown = [day, hours, minutes, seconds];
 const dateOut = new Date(Date.UTC(2025, 9, 1));
 
 const timerCountdown = () => {
   const timeOut = dateOut - new Date();
   setTimeout(() => {
-    // console.log(timeOut);
-    /**timeOut = le nombre de secondes restantes en timestamp
-     * obtenir les secondes : timeOut / 24 / 60 / 60 / 1000
-     * obtenir les minutes : timeOut / 24 / 60 / 1000
-     * heures : timeOut
-     *
+    /**
+     * timeOut = le nombre de secondes restantes en timestamp
+     * chaque counter retourne le nombre restant
+     * calculé à chaque seconde, le timer est dynamique
+     * et la récursivité fait que cette fonction se répète à l'infini
      */
     const dayCounter = Math.floor(timeOut / 1000 / (3600 * 24));
-    const hoursCounter = Math.floor(timeOut / 1000 / 3600 / dayCounter);
-    console.log(hoursCounter);
+    const hoursCounter = Math.floor(timeOut / 1000 / 3600) % 24;
+    const minutesCounter = Math.floor(timeOut / 1000 / 60) % 60;
+    const secondsCounter = Math.floor(timeOut / 1000) % 60;
 
-    day.innerHTML = dayCounter + "<em> Jours</em>";
-    hours.innerHTML = Math.floor(timeOut / 1000 / 3600) + "<em> Heures</em>";
-    minutes.innerHTML = hoursCounter + "<em> Minutes</em>";
+    day.innerHTML = dayCounter + "<em>Jours</em>";
+    hours.innerHTML = hoursCounter + "<em>Heures</em>";
+    minutes.innerHTML = minutesCounter + "<em>Minutes</em>";
     seconds.innerHTML =
-      Math.floor(timeOut / 1000 / (3600 * 24)) + "<em> Secondes</em>";
+      (secondsCounter == 0 ? "00" : secondsCounter) + "<em>Secondes</em>";
 
     timerCountdown();
   }, 1000);
