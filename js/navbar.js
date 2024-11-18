cb7a8c95956c1b7454f8211d0448cea85c189a0e
SuperHeraut <super.heraut.officiel@gmail.com>
2024-11-18 13:45:45 +0100

    fix: edit navbar parametres in js/navbar.js, styles/base/_typography.scss, styles/layout/_header.scss ; edit calculateur.html

--- js/navbar.js
+++ js/navbar.js
@@ -2,14 +2,22 @@ const NAV = document.querySelector("nav");
 const CROSS = document.getElementById("cross");
 const BURGER = document.getElementById("hamburger");
 
-function openNav(){
+let width = window.matchMedia("(min-width: 1366px)");
+
+function openNav() {
 	NAV.setAttribute("class", "display");
 	CROSS.setAttribute("class", "display");
 	BURGER.setAttribute("class", "no-display");
 }
 
-function closeNav(){
+function closeNav() {
 	NAV.setAttribute("class", "no-display");
 	CROSS.setAttribute("class", "no-display");
 	BURGER.setAttribute("class", "display");
+}
+
+if (width.matches) {
+	NAV.setAttribute("class", "display");
+	BURGER.setAttribute("class", "no-display");
+	CROSS.setAttribute("class", "no-display");
 }
