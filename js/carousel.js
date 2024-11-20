422bfc99bcba2bea5b2bd64aea88d6f111ae07a3
RobinDelbart05 <bamboomonkey05@gmail.com>
2024-11-20 16:07:14 +0100

    feat: ajout restriction largeur carousel pour Notebook

--- js/carousel.js
+++ js/carousel.js
@@ -9,37 +9,41 @@ slides.insertBefore(lastSlideClone, slides.children[0]);
 slides.appendChild(firstSlideClone);
 
 btnRight.addEventListener("click", () => {
-    compteur = compteur + 1;
-    slides.style.transition = "0.2s";
-    slides.style.transform = "translateX(" + compteur * -100 + "vw)";
-    if (compteur == 4) {
-        compteur = 1;
-        indexPoints[2].classList.remove("active");
-        setTimeout(() => {
-            slides.style.transition = "0s";
-            slides.style.transform = "translateX(" + compteur * -100 + "vw)";
-        }, 200);
-    } else {
-        indexPoints[compteur - 2].classList.remove("active");
+    if (window.innerWidth <= 1365) {
+        compteur = compteur + 1;
+        slides.style.transition = "0.2s";
+        slides.style.transform = "translateX(" + compteur * -100 + "vw)";
+        if (compteur == 4) {
+            compteur = 1;
+            indexPoints[2].classList.remove("active");
+            setTimeout(() => {
+                slides.style.transition = "0s";
+                slides.style.transform = "translateX(" + compteur * -100 + "vw)";
+            }, 200);
+        } else {
+            indexPoints[compteur - 2].classList.remove("active");
+        }
+        indexPoints[compteur - 1].classList.add("active");
     }
-    indexPoints[compteur - 1].classList.add("active");
 })
 
 btnLeft.addEventListener("click", () => {
-    compteur = compteur - 1;
-    slides.style.transition = "0.2s";
-    slides.style.transform = "translateX(" + compteur * -100 + "vw)";
-    if (compteur == 0) {
-        compteur = 3;
-        indexPoints[0].classList.remove("active");
-        setTimeout(() => {
-            slides.style.transition = "0s";
-            slides.style.transform = "translateX(" + compteur * -100 + "vw)";
-        }, 200);
-    } else {
-        indexPoints[compteur].classList.remove("active");
+    if (window.innerWidth <= 1365) {
+        compteur = compteur - 1;
+        slides.style.transition = "0.2s";
+        slides.style.transform = "translateX(" + compteur * -100 + "vw)";
+        if (compteur == 0) {
+            compteur = 3;
+            indexPoints[0].classList.remove("active");
+            setTimeout(() => {
+                slides.style.transition = "0s";
+                slides.style.transform = "translateX(" + compteur * -100 + "vw)";
+            }, 200);
+        } else {
+            indexPoints[compteur].classList.remove("active");
+        }
+        indexPoints[compteur - 1].classList.add("active");
     }
-    indexPoints[compteur - 1].classList.add("active");
 })
 
 window.onload = () => {
