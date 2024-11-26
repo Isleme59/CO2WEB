eeb871e686db08fdaae6c43afa8ab1d3931e38f2
gouttedooo <goudal.donovan@live.fr>
2024-11-26 11:20:35 +0100

    fix: countdown 1s avant chargement fixé

--- js/countdown.js
+++ js/countdown.js
@@ -5,32 +5,34 @@ const seconds = document.getElementById("countdown-seconds");
 const dateOut = new Date(Date.UTC(2025, 9, 1));
 
 const timerCountdown = () => {
-  const timeOut = dateOut - new Date();
   setTimeout(() => {
-    /**
-     * timeOut = le nombre de secondes restantes en timestamp
-     * chaque counter retourne le nombre restant
-     * calculé à chaque seconde, le timer est dynamique
-     * et la récursivité fait que cette fonction se répète à l'infini
-     */
-    const dayCounter = Math.floor(timeOut / 1000 / (3600 * 24));
-    const hoursCounter = Math.floor(timeOut / 1000 / 3600) % 24;
-    const minutesCounter = Math.floor(timeOut / 1000 / 60) % 60;
-    const secondsCounter = Math.floor(timeOut / 1000) % 60;
-
-    day.innerHTML = dayCounter + "<em>Jours</em>";
-    hours.innerHTML =
-      (hoursCounter <= 0 ? "0" + hoursCounter : hoursCounter) +
-      "<em>Heures</em>";
-    minutes.innerHTML =
-      (minutesCounter <= 9 ? "0" + minutesCounter : minutesCounter) +
-      "<em>Minutes</em>";
-    seconds.innerHTML =
-      (secondsCounter <= 9 ? "0" + secondsCounter : secondsCounter) +
-      "<em>Secondes</em>";
-
+    countdown();
     timerCountdown();
   }, 1000);
 };
 
+const countdown = () => {
+  const timeOut = dateOut - new Date();
+  /**
+   * timeOut = le nombre de secondes restantes en timestamp
+   * chaque counter retourne le nombre restant
+   * calculé à chaque seconde, le timer est dynamique
+   * et la récursivité fait que cette fonction se répète à l'infini
+   */
+  const dayCounter = Math.floor(timeOut / 1000 / (3600 * 24));
+  const hoursCounter = Math.floor(timeOut / 1000 / 3600) % 24;
+  const minutesCounter = Math.floor(timeOut / 1000 / 60) % 60;
+  const secondsCounter = Math.floor(timeOut / 1000) % 60;
+
+  day.innerHTML = dayCounter + "<em>Jours</em>";
+  hours.innerHTML =
+    (hoursCounter <= 0 ? "0" + hoursCounter : hoursCounter) + "<em>Heures</em>";
+  minutes.innerHTML =
+    (minutesCounter <= 9 ? "0" + minutesCounter : minutesCounter) +
+    "<em>Minutes</em>";
+  seconds.innerHTML =
+    (secondsCounter <= 9 ? "0" + secondsCounter : secondsCounter) +
+    "<em>Secondes</em>";
+};
+countdown();
 timerCountdown();
